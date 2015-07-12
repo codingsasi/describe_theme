ddae9a018de7da0f395a7caa67d8acc46d38a099
Abhai Sasidharan <abhaisasidharan@gmail.com>
2015-07-13 00:40:02 +0530

    Initial commit to theme

---
+++ templates/page--front.tpl.php
@@ -0,0 +1,15 @@
+<div id="container">
+  
+  <header class="siteHeader clearfix">
+      <h1 class="logo">Describe</h1>
+
+      <nav class="nav">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  
+</div>
